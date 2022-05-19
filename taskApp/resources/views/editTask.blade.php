df768c752c56f20b6b0a67b53bfa58c3c6cd164c
Chikuu98 <chiranjonline@gmail.com>
2022-05-19 08:11:25 +0530

    dark Signed-off-by: Chikuu98 <chiranjonline@gmail.com>

--- taskApp/resources/views/editTask.blade.php
+++ taskApp/resources/views/editTask.blade.php
@@ -8,8 +8,9 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
     <body class="bg-dark">
-        <div class="container">
-            <br><br><br><br><br><br><br><br>
+        <div class="container text-center">
+            <br><br><br><br><br><br>
+            <h1 class="text-light"><b>Update the Task</b></h1><br><br>
             <div class="text-center mt-5">
                 <form action="/updateTask" method="post">
                 {{csrf_field()}}
